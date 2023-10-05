3ea421bbd33bd20c1d08eacd456bfacaa376af39
tksk2884 <tksk2884@gmail.com>
2023-10-05 13:50:10 +0900

    Select Class Add Hover Event

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -359,7 +359,9 @@ export default class TestView extends Vue {
 
                         box-shadow: 0px 0px 1px #727272;
 
-                        :hover {
+                        &:hover {
+                            cursor: pointer;
+
                             background-color: #63ac9b;
                             color: #ffffff;
                         }
