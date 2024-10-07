92508214e22badaaa2bb549bd3ed971e56a950b7
tksk2884 <tksk2884@gmail.com>
2024-10-08 06:19:14 +0900

    loading관련 클래스, 애니메이션 LoadingIndicator로 이관

--- src/components/LoadingIndicator.vue
+++ src/components/LoadingIndicator.vue
@@ -1,8 +1,8 @@
 <template>
     <div :class="$style.index">
-        <div class="loadingio-spinner-rolling-5ax215itman">
-            <div class="ldio-r0kv4ehu2ae">
-                <div />
+        <div :class="$style.inner">
+            <div :class="$style.loading">
+                <span />
             </div>
         </div>
     </div>
@@ -20,5 +20,49 @@ export default class LoadingIndicater extends Vue {}
 .index {
     width: 100%;
     height: 100%;
+
+    > .inner {
+        width: 200px;
+        height: 200px;
+
+        margin-inline: auto;
+
+        display: block;
+        overflow: hidden;
+        background: none;
+
+        > .loading {
+            width: 100%;
+            height: 100%;
+
+            position: relative;
+
+            > span {
+                width: 50px;
+                height: 50px;
+
+                position: absolute;
+                top: 100px;
+                left: 100px;
+
+                z-index: 350;
+
+                border: 6px solid #666666;
+                border-top-color: transparent;
+                border-radius: 50%;
+
+                animation: rotate 0.5s linear infinite;
+            }
+        }
+    }
+
+    @keyframes rotate {
+        0% {
+            transform: translate(-50%, -50%) rotate(0deg);
+        }
+        100% {
+            transform: translate(-50%, -50%) rotate(360deg);
+        }
+    }
 }
 </style>
